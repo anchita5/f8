80229e9f81235aeda848cf6ee8aa43311422d198
Anchita Kakria <anchita585@gmail.com>
2024-03-29 18:04:22 -0400

    added checking for arguments error

--- f8.cpp
+++ f8.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
 struct MyData
 {
 	int howMany;
@@ -8,6 +11,25 @@ struct MyData
 };
 int main(int argc, char* argv[])
 {
+	if (argc != 5)
+	{
+		printf("Error");
+		return -1;
+	}
+
+	if ((strlen(argv[2]) > 20))
+	{
+		printf("Error: Text input is longer than 20 characters");
+		return -1;
+	}
+	else if ((strlen(argv[3]) > 20))
+	{
+		printf("Error: Destination path longer than 20 characters");
+	}
+	else if ((strlen(argv[4]) > 20))
+	{
+		printf("Error: File name greater than 20 characters");
+	}
 
 	return 0;
 }
